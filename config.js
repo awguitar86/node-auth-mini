215f363bb88a934f31c7e1827ed9484e0b0b609f
Austin Wright <awguitar86@gmail.com>
2018-01-15 19:20:56 -0700

    created the config.js and wrote the mdule.exports object

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    domain: "awguitar86.auth0.com",
+    clientID: "3XNi8BgmEWxod79GPhzDMprwxkcME9Om",
+    clientSecret: "d187FII_hbvWqxhPvWbwWddVzI0Kbswg0MQhyhpdjDNoUck3HMD_wmyUHWnwv5Cf",
+
+};
