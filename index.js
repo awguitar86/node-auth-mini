59ada64e4df944c36ac3dbc7ac79f4969fe46684
Austin Wright <awguitar86@gmail.com>
2018-01-15 19:39:34 -0700

    configure app to use sessions

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const passport = require('passport');
 const strategy = require("./strategy");
 
 const app = express();
+app.use( session({
+    secret: 'sup dude',
+    resave: false,
+    saveUninitialized: false
+}));
+// app.use( passport.initialize() );
+// app.use( passport.session() );
+// passport.use( startegy );
 
 const port = 3000;
 app.listen( port, () => { console.log(`Server listening on port ${port}`); } );
